d03e5e93ee3c9b290fcce5ecdc64c8d80ac1680b
matt <matt5234@gmail.com>
2016-04-04 14:54:53 -0500

    tagging feature added

---
+++ blogger/app/controllers/tags_controller.rb
@@ -0,0 +1,18 @@
+class TagsController < ApplicationController
+
+	def show 
+		@tag = Tag.find(params[:id])
+	end
+
+	def index
+		@tags = Tag.all
+	end
+
+	def destroy
+		@tag = tag.find(params[:id])
+		@tag.destroy
+
+		flash.notice = "Tag Deleted"
+		redirect_to tags_path
+	end
+end
